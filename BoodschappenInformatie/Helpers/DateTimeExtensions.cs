8da04aeaf2550074dd1840e70ec2e68c96f8b675
Kees Hiemstra <chi@xs4all.nl>
2018-05-14 18:54:29 +0200

    Add calendar per week as page

---
+++ BoodschappenInformatie/Helpers/DateTimeExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoodschappenInformatie.Helpers
+{
+	public static class DateTimeExtensions
+	{
+		public static string WeekNumberString(this DateTime date)
+		{
+			DateTimeFormatInfo dfi = DateTimeFormatInfo.CurrentInfo;
+			Calendar cal = dfi.Calendar;
+
+			int year = date.Year;
+			int week = cal.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+
+			if (week == 52 && date.Month == 1)
+			{
+				year--;
+			}
+			else if (week == 53)
+			{
+				if (date.Month == 12 && date.DayOfWeek <= DayOfWeek.Wednesday)
+				{
+					week = 1;
+					year++;
+				}
+				else if (date.Month == 1 && date.DayOfWeek >= DayOfWeek.Thursday)
+				{
+					year--;
+				}
+			}
+
+			string weekStr = week.ToString();
+
+			return $"{year}.{("0" + weekStr).Substring(weekStr.Length - 1)}";
+		}
+	}
+}
---
+++ BoodschappenInformatie/Pages/KassaBonnen/CalendarWeeks.cshtml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BoodschappenInformatie.Data;
+using BoodschappenInformatie.Models;
+using BoodschappenInformatie.Helpers;
+
+namespace BoodschappenInformatie.Pages.KassaBonnen
+{
+	public class CalendarWeeksModel : PageModel
+	{
+		private readonly BoodschappenInformatie.Data.BoodschappenContext _context;
+
+		public CalendarWeeksModel(BoodschappenInformatie.Data.BoodschappenContext context)
+		{
+			_context = context;
+		}
+
+		public IList<KassaBonItem> KassaBonItem { get; set; }
+		public IEnumerable<string> DateHeader { get; set; }
+		public IEnumerable<string> Descriptions { get; set; }
+
+		public async Task OnGetAsync()
+		{
+			DateHeader = await _context.KassaBonItems
+				.Select(WeekNumber => WeekNumber.KassaBon.BonDate.WeekNumberString())
+				.Distinct()
+				.OrderByDescending(x => x)
+				.ToListAsync();
+
+			Descriptions = await _context.KassaBonItems
+				.Select(x => x.Boodschap.Description)
+				.Distinct()
+				.OrderBy(x => x)
+				.ToListAsync();
+
+			var List = from x in _context.KassaBonItems
+								 orderby x.KassaBon.Description
+								 select new
+								 {
+									 Description = x.Boodschap.Description,
+									 WeekName = x.KassaBon.BonDate.WeekNumberString(),
+									 Quantity = x.Hoeveelheid
+								 };
+
+			KassaBonItem = await _context.KassaBonItems
+					.AsNoTracking()
+					.Include(k => k.Boodschap)
+					.Include(k => k.KassaBon)
+					.ToListAsync();
+		}
+	}
+
+	public static class CalendarWeeksModelExtensions
+	{
+		public static string GetCalendarCell(this CalendarWeeksModel model, string shopDescription, string weekNumber)
+		{
+			string result = string.Empty;
+
+			var calendarDesciptions = model.KassaBonItem.Where(x => x.Boodschap.Description == shopDescription);
+			if (calendarDesciptions == null) { return result; }
+
+			var calendarCell = calendarDesciptions.Where(x => x.KassaBon.BonDate.WeekNumberString() == weekNumber);
+			if (calendarCell == null) { return result; }
+
+			decimal sum = calendarCell.Sum(x => x.Hoeveelheid).Value;
+			if (sum == 0) { return result; }
+
+			string package = calendarCell.Select(x => x.Boodschap.Package)
+				.Distinct() //Resolve when it results as more lines
+				.Single();
+			if (package == "St")
+			{
+				result = ((int)sum).ToString();
+			}
+			else
+			{
+				result = sum.ToString();
+			}
+
+			return result;
+		}
+	}
+}
