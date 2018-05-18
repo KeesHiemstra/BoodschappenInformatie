9bc9b8c3bbb712624d9b0d33fc5591d38cea2c30
Kees Hiemstra <chi@xs4all.nl>
2018-05-18 11:53:53 +0200

    Added Match() StringExtensions.

--- BoodschappenInformatie/Helpers/StringExtensions.cs
+++ BoodschappenInformatie/Helpers/StringExtensions.cs
@@ -7,10 +7,36 @@ namespace BoodschappenInformatie.Helpers
 {
 	public static class StringExtensions
 	{
+		#region ShortAccountNumber()
+		/// <summary>
+		/// Return the last 10 characters of the Account.
+		/// </summary>
+		/// <param name="account"></param>
+		/// <returns></returns>
 		public static string ShortAccountNumber(this string account)
 		{
 			var result = account.Substring(account.Length - 10);
 			return result;
 		}
+		#endregion
+
+		#region Match()
+		/// <summary>
+		/// Returns true or false if the input matches the pattern.
+		/// </summary>
+		/// <param name="model"></param>
+		/// <param name="pattern"></param>
+		/// <param name="match"></param>
+		/// <returns></returns>
+		public static bool Match(this string model, string pattern, bool match = true)
+		{
+			bool result = false;
+
+			Regex regex = new Regex(pattern);
+			result = regex.IsMatch(model) == match;
+
+			return result;
+		}
+		#endregion
 	}
 }
