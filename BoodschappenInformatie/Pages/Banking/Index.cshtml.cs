515acddb6684d871e73c032251054103f81e9e8b
Kees Hiemstra <chi@xs4all.nl>
2018-05-26 11:04:56 +0200

    Added comment

--- BoodschappenInformatie/Pages/Banking/Index.cshtml.cs
+++ BoodschappenInformatie/Pages/Banking/Index.cshtml.cs
@@ -22,8 +22,15 @@ namespace BoodschappenInformatie.Pages.Banking
 
 		public IList<Bank> Bank { get; set; }
 
+		/// <summary>
+		/// Added search options in the index page.
+		/// </summary>
+		/// <param name="searchMonth"></param>
+		/// <param name="searchTally"></param>
+		/// <returns></returns>
 		public async Task OnGetAsync(string searchMonth, string searchTally)
 		{
+			//Query for Bank
 			var _Bank = from db in _context.BankRecords
 									select db;
 
